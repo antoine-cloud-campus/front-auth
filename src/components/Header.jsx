fee1402632bede0727279abffdf9d57a23e73a79
Nubunduram <ceantoine@hotmail.fr>
2025-05-14 11:34:42 +0200

    Fix: Header links available for connected user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,40 @@
+import { useState, useEffect } from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
-import { NavLink } from "react-router";
+import { NavLink, useLocation } from "react-router";
 import "../assets/styles/Header.css";
+
 function Header() {
+  const location = useLocation();
+
+  const getValidToken = () => {
+    const auth = JSON.parse(localStorage.getItem("auth"));
+    const isValid = auth && new Date(auth.expiresAt) > new Date();
+    return isValid ? auth.token : null;
+  };
+
+  const [token, setToken] = useState(getValidToken);
+
+  useEffect(() => {
+    setToken(getValidToken());
+  }, [location]);
+
   return (
     <Navbar bg="light" data-bs-theme="light">
       <Container>
         <Nav className="ms-auto">
-          <Nav.Link as={NavLink} to="/">
-            Accueil
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/offres/publiques">
-            Offres Publiques
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/offres/professionnelles">
-            Offres Professionnelles
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/inscription">
-            Inscription
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/connexion">
-            Connexion
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/deconnexion">
-            Déconnexion
-          </Nav.Link>
+          <Nav.Link as={NavLink} to="/">Accueil</Nav.Link>
+          <Nav.Link as={NavLink} to="/offres/publiques">Offres Publiques</Nav.Link>
+          <Nav.Link as={NavLink} to="/offres/professionnelles">Offres Professionnelles</Nav.Link>
+
+          {token
+            ?
+            <Nav.Link as={NavLink} to="/deconnexion">Déconnexion</Nav.Link>
+            :
+            <>
+              <Nav.Link as={NavLink} to="/inscription">Inscription</Nav.Link>
+              <Nav.Link as={NavLink} to="/connexion">Connexion</Nav.Link>
+            </>}
+
         </Nav>
       </Container>
     </Navbar>
